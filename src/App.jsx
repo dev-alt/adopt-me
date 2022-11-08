33824f955a0e7a6bd33aff2bbda6e9854d646746
Andrew <andrew.logan@hotmail.com>
2022-11-09 01:37:39 +1300

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { render } from "react-dom";
-import Pet from "./Pet";
-
-
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div onClick={() => {}}>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="HCockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
-      </div>
-  )
-  } 
+      <SearchParams />
+    </div>
+  );
+};
 
 render(<App />, document.getElementById("root"));
